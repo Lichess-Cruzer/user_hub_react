ea62398d75b11718ef462e0b501b4221b03c3f0d
Lichess-Cruzer <alexhenige@gmail.com>
2021-06-12 13:25:48 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+    Header,
+    UserPosts
+  } from './components';
+
+  import {
+    getUsers,
+    getPostsByUser
+  } from './api';
+
+
+  const App = () => {
+    const [userList, setUserList] = useState([]);
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userPosts, setUserPosts] = useState([]); 
+  
+    useEffect(() => {
+      getUsers()
+        .then(users => {
+          setUserList(users)
+        })
+        .catch(error => {
+          console.log(error)
+        });
+    }, []);
+
+    useEffect(() => {
+        if (!currentUser) {
+          setUserPosts([]);
+          return;
+        }
+    
+        getPostsByUser(currentUser.id)
+          .then(posts => {
+            setUserPosts(posts);
+          })
+          .catch(error => {
+            console.log(error)
+          });
+      }, [currentUser]);
+  
+      return (
+        <div id="App">
+          <Header
+            userList={ userList }
+            currentUser={ currentUser }
+            setCurrentUser={ setCurrentUser } />
+          {
+            currentUser
+            ? <UserPosts
+                userPosts={ userPosts }
+                currentUser={ currentUser } />
+            : null
+          }
+    
+        </div>
+      );
+  }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
